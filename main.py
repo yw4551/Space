f1828a25b242294991a0dd70c7436f18c7772c3e
Yitzi Weiner <104987033+yw4551@users.noreply.github.com>
2026-02-02 10:11:41 +0200

    Add dynamic packet routing with smart_send_packet
    Introduced a pathfinding algorithm to determine relay routes between satellites and Earth, enabling dynamic packet transmission via the new smart_send_packet function. Updated imports and refactored main to use the new routing logic for improved flexibility.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from space_network import *
+from space_network_lib import *
 import time
+from collections import deque
 
 class RelayPacket(Packet):
     def __init__(self, packet_to_relay, sender, proxy):
@@ -24,11 +25,50 @@ class BrokenConnectionError(Exception):
     pass
 
 sn = SpaceNetwork(level=3)
+earth = Satellite("earth", 0)
 sat1 = Satellite("sat1", 100)
 sat2 = Satellite("sat2", 200)
-earth = Satellite("earth", 0)
-p_final = Packet("Hello from earth", sat1, sat2)
+sat3 = Satellite("sat3", 300)
+sat4 = Satellite("sat4", 400)
+packet = Packet("Hello from earth", sat3, sat4)
+relay1 = RelayPacket(packet, sat2, sat3)
+relay2 = RelayPacket(relay1, sat1, sat2)
+relay3 = RelayPacket(relay2, earth, sat1)
+p_final = Packet("Hello from earth", sat1, sat4)
 p_earth_to_sat1 = RelayPacket(p_final, earth, sat1)
+locations = {"earth": 0, "sat1": 100, "sat2": 200, "sat3": 300, "sat4": 400}
+
+def find_path(entities, start, end):
+    queue = deque([[start]])
+    visited = {start}
+    
+    while queue:
+        path = queue.popleft()
+        node = path[-1]
+        
+        if node == end:
+            return path
+            
+        for entity in entities:
+            if entity not in visited and abs(node.distance_from_earth - entity.distance_from_earth) <= 150:
+                visited.add(entity)
+                queue.append(path + [entity])
+    return None
+
+def smart_send_packet(entities, initial_packet):
+    path = find_path(entities, initial_packet.sender, initial_packet.receiver)
+    
+    if not path:
+        print("No route found!")
+        return
+
+    print(f"Path found: {' -> '.join([e.name for e in path])}")
+    current_packet = Packet(initial_packet.data, path[-2], path[-1])
+    
+    for i in range(len(path) - 3, -1, -1):
+        current_packet = RelayPacket(current_packet, path[i], path[i+1])
+    
+    transmission_attempt(current_packet)
 
 def transmission_attempt(packet):
     while True:
@@ -50,7 +90,13 @@ def transmission_attempt(packet):
         break
 
 def main():
-    transmission_attempt(p_earth_to_sat1)
+    all_entities = [earth, sat1, sat2, sat3, sat4]
+    p_request = Packet("Hello From Earth!", earth, sat4)
+    
+    try:
+        smart_send_packet(all_entities, p_request)
+    except BrokenConnectionError:
+        print("Transmission failed")
 
 if __name__ == "__main__":
     main()
